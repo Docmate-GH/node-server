052042c0e842a22fda1fe9345ecfdd6da82bee0e
Randy <randypriv@gmail.com>
2020-08-09 22:37:14 +0800

    web template

--- src/index.ts
+++ src/index.ts
@@ -16,60 +16,61 @@ export interface AppReq extends express.Request {
 
 const app = express()
 
-  const PORT = 3000
+const PORT = 3000
 
-  const client = createClient({
-    url: 'http://localhost:8080/v1/graphql',
-    fetch,
-    requestPolicy: 'network-only',
-    fetchOptions: {
-      headers: {
-        'x-hasura-admin-secret': 'myadminsecretkey'
-      }
+const client = createClient({
+  url: 'http://localhost:8080/v1/graphql',
+  fetch,
+  requestPolicy: 'network-only',
+  fetchOptions: {
+    headers: {
+      'x-hasura-admin-secret': 'myadminsecretkey'
     }
-  })
+  }
+})
 
-  const appService = new AppService(client)
+const appService = new AppService(client)
 
-  // views
-  nunjucks.configure('views', {
-    express: app,
-    autoescape: false
-  })
-  app.set('views', path.resolve(__dirname, '../views'))
+// views
+nunjucks.configure('views', {
+  express: app,
+  autoescape: false
+})
+app.set('views', path.resolve(__dirname, '../views'))
 
-  app.use(require('express-session')({ secret: 'foo' }))
-  app.use(require('cookie-parser')())
+app.use(require('express-session')({ secret: 'foo' }))
+app.use(require('cookie-parser')())
 
-  app.use(require('body-parser').json())
-  app.use(require('body-parser').urlencoded({ extended: false }))
+app.use(require('body-parser').json())
+app.use(require('body-parser').urlencoded({ extended: false }))
 
-  app.use('/static', express.static(path.resolve(__dirname, '../static')))
+app.use('/static', express.static(path.resolve(__dirname, '../static')))
 
-  app.use((req: AppReq, res, next) => {
-    // req.user = FAKE_USER
-    req.appService = appService
-    next()
-  })
+app.use((req: AppReq, res, next) => {
+  // req.user = FAKE_USER
+  req.appService = appService
+  next()
+})
 
-  app.get('/' , async (req: AppReq, res) => {
-    res.send('works')
-  })
+app.get('/login', (req, res) => {
+  res.send('hi')
+})
 
-  app.get('/login', (req, res) => {
-    res.send('hi')
-  })
+app.get('/docs/:docId', doc.home)
 
-  app.get('/docs/:docId', doc.home)
+app.get('/docs/:docId/:fileName', doc.renderFile)
 
-  app.get('/docs/:docId/:fileName', doc.renderFile)
+app.post('/handler/actions/signUp', signUpAction)
+app.post('/handler/actions/signIn', signinAction)
+app.post('/handler/actions/createTeam', createTeam)
+app.post('/handler/actions/joinTeam', joinTeam)
+app.post('/handler/actions/revokeInviteId', revokeInviteId)
 
-  app.post('/handler/actions/signUp', signUpAction)
-  app.post('/handler/actions/signIn', signinAction)
-  app.post('/handler/actions/createTeam', createTeam)
-  app.post('/handler/actions/joinTeam', joinTeam)
-  app.post('/handler/actions/revokeInviteId', revokeInviteId)
 
-  app.listen(PORT, () => {
-    console.log('running at', PORT)
-  })
+app.get('/*', async (req: AppReq, res) => {
+  res.render('index.html')
+})
+
+app.listen(PORT, () => {
+  console.log('running at', PORT)
+})
